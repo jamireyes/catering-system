40ec97c029e702b63fc20e410c75a272a9710859
jamireyes <35447073+jamireyes@users.noreply.github.com>
2022-11-29 15:11:38 +0800

    Removed commented lines
    Removed commented lines from the app.blade.php and welcome.blade.php files.

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -15,9 +15,6 @@
     <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,300;0,400;0,500;0,600;0,700;0,800;0,900;1,100;1,200;1,300;1,400;1,500;1,600;1,700;1,800;1,900&display=swap" rel="stylesheet">
     <link href="https://maxcdn.bootstrapcdn.com/font-awesome/latest/css/font-awesome.min.css" rel="stylesheet">
     
-    {{-- <link href="{{ asset('paper') }}/css/bootstrap.min.css" rel="stylesheet" />
-    <link href="{{ asset('paper') }}/css/paper-dashboard.css?v=2.0.0" rel="stylesheet" />
-    <link href="{{ asset('assets') }}/css/custom.css" rel="stylesheet" /> --}}
     <link href="{{ asset('css/all.css') }}" rel="stylesheet" />
 
     <link href="https://cdn.datatables.net/1.10.21/css/dataTables.bootstrap4.min.css" rel="stylesheet">
